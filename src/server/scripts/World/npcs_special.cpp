26d668cdcf02539eb582903f362b0db06cfae60c
wintersong-wow <wintersongwow@gmail.com>
2025-01-23 23:39:07 +0300

    Training Dummy improvements
    Cherry-picked from Project Epoch.
    
    Training dummies now drop combat with a target after 15 seconds of not being attacked, up from 5 seconds.
    
    Training dummies with max health higher than 1 (Heroic Training Dummy) can now be damaged down to 1 hp and now regenerate health once they leave combat with all targets.

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1554,17 +1554,19 @@ struct npc_training_dummy : NullCreatureAI
 
     void JustEnteredCombat(Unit* who) override
     {
-        _combatTimer[who->GetGUID()] = 5s;
+        _combatTimer[who->GetGUID()] = 15s;
+        me->SetRegenerateHealth(false);
     }
 
     void DamageTaken(Unit* attacker, uint32& damage, DamageEffectType damageType, SpellInfo const* /*spellInfo = nullptr*/) override
     {
-        damage = 0;
+        if (damage >= me->GetHealth() - 1)
+            damage = me->GetHealth() - 1;
 
         if (!attacker || damageType == DOT)
             return;
 
-        _combatTimer[attacker->GetGUID()] = 5s;
+        _combatTimer[attacker->GetGUID()] = 15s;
     }
 
     void UpdateAI(uint32 diff) override
@@ -1574,11 +1576,14 @@ struct npc_training_dummy : NullCreatureAI
             itr->second -= Milliseconds(diff);
             if (itr->second <= 0s)
             {
-                // The attacker has not dealt any damage to the dummy for over 5 seconds. End combat.
+                // The attacker has not dealt any damage to the dummy for over 15 seconds. End combat.
                 auto const& pveRefs = me->GetCombatManager().GetPvECombatRefs();
                 auto it = pveRefs.find(itr->first);
                 if (it != pveRefs.end())
                     it->second->EndCombat();
+
+                if (!me->GetCombatManager().HasCombat())
+                    me->SetRegenerateHealth(true);
 
                 itr = _combatTimer.erase(itr);
             }
