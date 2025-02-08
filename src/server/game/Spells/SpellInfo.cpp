c8bd4eda74ea20c40e1e62a8e95ac37f28600f1e
wintersong-wow <wintersongwow@gmail.com>
2025-02-08 22:06:49 +0300

    0x00000800 is the common SpellFamilyFlag[2] for Seal spells now.

--- src/server/game/Spells/SpellInfo.cpp
+++ src/server/game/Spells/SpellInfo.cpp
@@ -2186,8 +2186,7 @@ void SpellInfo::_LoadSpellSpecific()
             case SPELLFAMILY_PALADIN:
             {
                 // Collection of all the seal family flags. No other paladin spell has any of those.
-                if (SpellFamilyFlags[1] & 0x26000C00
-                    || SpellFamilyFlags[0] & 0x0A000000 || SpellFamilyFlags[2] & 0x00000200) // 0x00000200 is "Seal of the Crusader"
+                if (SpellFamilyFlags[2] & 0x00000800)
                     return SPELL_SPECIFIC_SEAL;
 
                 if (SpellFamilyFlags[0] & 0x00002190)
