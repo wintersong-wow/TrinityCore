a390dbe3c1bfbf78aeb46401cfde5703f8afc99f
wintersong-wow <wintersongwow@gmail.com>
2025-03-29 23:43:06 +0300

    [CORE/SPELLS] Reflective Shield now affects target, not just the caster.

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -881,6 +881,11 @@ class spell_pri_power_word_shield_aura : public AuraScript
     void ReflectDamage(AuraEffect* aurEff, DamageInfo& dmgInfo, uint32& absorbAmount)
     {
         Unit* target = GetTarget();
+        Unit* caster = GetCaster();
+
+        if (!caster)
+            return;
+
         if (dmgInfo.GetAttacker() == target)
             return;
 
@@ -888,7 +893,7 @@ class spell_pri_power_word_shield_aura : public AuraScript
         if (dmgInfo.GetSpellInfo() && dmgInfo.GetSpellInfo()->Id == SPELL_PRIEST_REFLECTIVE_SHIELD_TRIGGERED)
             return;
 
-        if (AuraEffect* talentAurEff = target->GetAuraEffectOfRankedSpell(SPELL_PRIEST_REFLECTIVE_SHIELD_R1, EFFECT_0))
+        if (AuraEffect* talentAurEff = caster->GetAuraEffectOfRankedSpell(SPELL_PRIEST_REFLECTIVE_SHIELD_R1, EFFECT_0))
         {
             CastSpellExtraArgs args(aurEff);
             args.AddSpellBP0(CalculatePct(absorbAmount, talentAurEff->GetAmount()));
