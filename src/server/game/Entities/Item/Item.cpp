b42544164f15333c208671e01ebe902fd5db1ba9
wintersong-wow <wintersongwow@gmail.com>
2025-09-06 19:55:29 +0300

    Transmog unit field index changed, Enchant unit field appended to Item.

--- src/server/game/Entities/Item/Item.cpp
+++ src/server/game/Entities/Item/Item.cpp
@@ -38,6 +38,14 @@
 #include "World.h"
 #include "WorldPacket.h"
 #include "WorldSession.h"
+#include "Transmogrification.h"
+
+void Item::SetBinding(bool val)
+{
+    ApplyModFlag(ITEM_FIELD_FLAGS, ITEM_FIELD_FLAG_SOULBOUND, val);
+    if (val)
+        Transmogrification::instance().AddToCollection(GetOwner(), this);
+}
 
 void AddItemsSetItem(Player* player, Item* item)
 {
@@ -364,6 +372,7 @@ void Item::SaveToDB(CharacterDatabaseTransaction trans)
             stmt->setString(++index, m_text);
             // @tswow-begin (Using Rochet2/Transmog)
             stmt->setUInt32(++index, transmog);
+            stmt->setUInt32(++index, enchant);
             // @tswow-end
             stmt->setUInt32(++index, guid);
 
@@ -503,7 +512,8 @@ bool Item::LoadFromDB(ObjectGuid::LowType guid, ObjectGuid owner_guid, Field* fi
     }
 
     // @tswow-begin (Using Rochet2/Transmog)
-    transmog = fields[15].GetUInt32();
+    transmog = fields[16].GetUInt32();
+    enchant  = fields[17].GetUInt32();
     // @tswow-end
 
     return true;
